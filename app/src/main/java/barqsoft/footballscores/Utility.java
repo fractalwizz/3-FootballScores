81e3cdc5320bf8fe7f1319e863f868db6c7fb0f6
William Young <fractalwizz@hotmail.com>
2016-02-07 22:42:53 -0600

    Commit #5 - Crest Update 1

--- app/src/main/java/barqsoft/footballscores/Utility.java
+++ app/src/main/java/barqsoft/footballscores/Utility.java
@@ -52,19 +52,70 @@ public class Utility {
         if (teamname == null) { return R.drawable.no_icon; }
 
         switch (teamname) {
-            //This is the set of icons that are currently in the app. Feel free to find and add more
-            //as you go.
             //TODO add more crests
+            case "1. FC Heidenheim 1846" : return R.drawable.fc_heidenheim;
+            case "1. FC Kaiserslautern" : return R.drawable.fc_kaiserslautern;
+            case "1. FC Nürnberg" : return R.drawable.fc_nurnberg;
+            case "1. FC Union Berlin" : return R.drawable.fc_union_berlin;
+            case "1. FSV Mainz 05" : return R.drawable.fsv_mainz;
+            case "ACF Fiorentina" : return R.drawable.acf_fiorentina;
+            case "Arminia Bielefeld" : return R.drawable.arminia_bielefeld;
             case "Arsenal London FC" : return R.drawable.arsenal;
-            case "Manchester United FC" : return R.drawable.manchester_united;
-            case "Swansea City" : return R.drawable.swansea_city_afc;
-            case "Leicester City" : return R.drawable.leicester_city_fc_hd_logo;
+            case "Aston Villa FC" : return R.drawable.aston_villa;
+            case "Athletic Club" : return R.drawable.athletic_club;
+            case "Bayer Leverkusen" : return R.drawable.bayer_leverkusen;
+            case "Bologna FC" : return R.drawable.bologna_fc;
+            case "Bor. Mönchengladbach" : return R.drawable.bor_monchengladbach;
+            case "Borussia Dortmund" : return R.drawable.borussia_dordmund;
+            case "Club Atlético de Madrid" : return R.drawable.club_atletico;
+            case "Crystal Palace FC" : return R.drawable.crystal_palace_fc;
+            case "Eintracht Frankfurt" : return R.drawable.eintracht_frankfurt;
             case "Everton FC" : return R.drawable.everton_fc_logo1;
-            case "West Ham United FC" : return R.drawable.west_ham;
-            case "Tottenham Hotspur FC" : return R.drawable.tottenham_hotspur;
-            case "West Bromwich Albion" : return R.drawable.west_bromwich_albion_hd_logo;
-            case "Sunderland AFC" : return R.drawable.sunderland;
+            case "FC Augsburg" : return R.drawable.fc_augsburg;
+            case "FC Bayern München" : return R.drawable.bayern_munchen;
+            case "FC Ingolstadt 04" : return R.drawable.fc_ingolstadt;
+            case "FC Schalke 04" : return R.drawable.fc_schalke;
+            case "Fortuna Düsseldorf" : return R.drawable.fortuna_dusseldorf;
+            case "Genoa CFC" : return R.drawable.genoa_cfc;
+            case "Getafe CF" : return R.drawable.getafe_cf;
+            case "Hannover 96" : return R.drawable.hannover_96;
+            case "Hertha BSC" : return R.drawable.hertha_bsc;
+            case "Leicester City FC" : return R.drawable.leicester_city_fc_hd_logo;
+            case "Liverpool FC" : return R.drawable.liverpool_fc;
+            case "Málaga CF" : return R.drawable.malaga_cf;
+            case "Manchester City" : return R.drawable.manchester_city;
+            case "Manchester City FC" : return R.drawable.manchester_city;
+            case "Manchester United FC" : return R.drawable.manchester_united;
+            case "MSV Duisburg" : return R.drawable.msv_duisburg;
+            case "Newcastle United FC" : return R.drawable.newcastle_united;
+            case "Norwich City FC" : return R.drawable.norwich_city;
+            case "Rayo Vallecano de Madrid" : return R.drawable.rayo_vallecano;
+            case "RC Deportivo La Coruna" : return R.drawable.rc_deportivo;
+            case "RCD Espanyol" : return R.drawable.rcd_espanyol;
+            case "Real Sociedad de Fútbol" : return R.drawable.real_sociedad;
+            case "SC Freiburg" : return R.drawable.sc_freiburg;
+            case "SC Paderborn 07" : return R.drawable.sc_paderborn07;
+            case "SD Eibar" : return R.drawable.sd_eibar;
+            case "Southampton FC" : return R.drawable.southampton_fc;
+            case "Sporting Gijón" : return R.drawable.sporting_gijon;
+            case "SS Lazio" : return R.drawable.ss_lazio;
             case "Stoke City FC" : return R.drawable.stoke_city;
+            case "Sunderland AFC" : return R.drawable.sunderland;
+            case "SV Sandhausen" : return R.drawable.sv_sandhausen;
+            case "Swansea City" : return R.drawable.swansea_city_afc;
+            case "Swansea City FC" : return R.drawable.swansea_city_afc;
+            case "Tottenham Hotspur FC" : return R.drawable.tottenham_hotspur;
+            case "TSV 1860 München" : return R.drawable.tsv_munchen;
+            case "UD Las Palmas" : return R.drawable.ud_las_palmas;
+            case "VfB Stuttgart" : return R.drawable.vfb_stuttgart;
+            case "VfL Bochum" : return R.drawable.vfl_bochum;
+            case "VfL Wolfsburg" : return R.drawable.vfl_wolfsburg;
+            case "Villarreal CF" : return R.drawable.villarreal_cf;
+            case "Watford FC" : return R.drawable.watford_fc;
+            case "Werder Bremen" : return R.drawable.werder_bremen;
+            case "West Bromwich Albion" : return R.drawable.west_bromwich_albion_hd_logo;
+            case "West Bromwich Albion FC" : return R.drawable.west_bromwich_albion_hd_logo;
+            case "West Ham United FC" : return R.drawable.west_ham;
             default: return R.drawable.no_icon;
         }
     }
